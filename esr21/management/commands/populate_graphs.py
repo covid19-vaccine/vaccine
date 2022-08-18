e939ebe3438bf5c2bc4428e93a0381d3e4aee259
MoffatMore <mofenyimoffat@gmail.com>
2022-08-18 18:10:08 +0200

    update populate graphs command

--- esr21/management/commands/populate_graphs.py
+++ esr21/management/commands/populate_graphs.py
@@ -15,7 +15,7 @@ from esr21_reports.views.adverse_events import (
     AdverseEventRecordViewMixin, SeriousAdverseEventRecordViewMixin)
 from esr21_reports.views.psrt_mixins import ScreeningReportsViewMixin
 from esr21_reports.views.site_helper_mixin import SiteHelperMixin
-from esr21_reports.views.study_progres import StudyProgressView
+from esr21_reports.views import HomeView
 
 
 class Command(BaseCommand):
@@ -24,16 +24,6 @@ class Command(BaseCommand):
     siteHelper = SiteHelperMixin()
 
     def handle(self, *args, **kwargs):
-        # DashboardStatistics.objects.all().delete()
-        # self.populate_screening_data()
-        # self.populate_enrollement_data()
-        # self.populate_vaccination_data()
-        # self.populate_enrollement_enrollement_with_conhorts()
-        # self.populate_vaccinate()
-        # self.populate_demographics()
-        # self.populate_genaral_statistics()
-        # self.populate_vaccine_enrollments()
-        # self.populate_pregnancy_statistics()
         self.populate_progress_reports()
 
     def populate_screening_data(self):
@@ -174,12 +164,12 @@ class Command(BaseCommand):
         )
 
     def populate_progress_reports(self):
-        progress_reports = StudyProgressView()
+        progress_reports = HomeView()
         for site in self.siteHelper.sites_names:
             site_id = self.siteHelper.get_site_id(site)
             screenings = progress_reports.get_site_screening(site_id=site_id)
             homologous_enrollments = progress_reports.cohort_participants(site_id=site_id)
-            heterologous_enrollments = progress_reports.heterologous_enrols
+            heterologous_enrollments = progress_reports.cohort(site_id=site_id)
 
             screening_defaults = {
                 'dose1':  screenings[0],
@@ -195,28 +185,34 @@ class Command(BaseCommand):
 
             sub_cohort, main_cohort, totals = homologous_enrollments
             enrol_defaults = {
+                    'site': site,
                     'series': 'homologous',
+                    'main_cohort': main_cohort,
+                    'sub_cohort': sub_cohort,
+                    'total': totals
+            }
+
+            EnrollmentStatistics.objects.update_or_create(
+                site_series=f'{site}_homologous',
+                defaults=enrol_defaults
+            )
+
+            sub_cohort, main_cohort, totals = heterologous_enrollments
+            enrol_defaults = {
+                    'site': site,
+                    'series': 'heterologous',
                     'main_cohort': main_cohort,
                     'sub_cohort': sub_cohort,
                     'total': totals
                 }
             EnrollmentStatistics.objects.update_or_create(
-                site=site,
+                site_series=f'{site}_heterologous',
                 defaults=enrol_defaults
             )
 
-            # for heterologous enrollments
-            # for cohort in heterologous_enrollments:
-            #     enrol_defaults = {
-            #         'cohort_totals': cohort[1],
-            #         'cohort': cohort[0]
-            #     }
-            #     EnrollmentStatistics.objects.update_or_create(
-            #         site=site,
-            #         defaults=enrol_defaults
-            #     )
             first, second, booster, total = progress_reports.homologous_vaccinations(site_id)
             vacc_defaults = {
+                'site': site,
                 'dose_1': first,
                 'dose_2': second,
                 'dose_3': booster,
@@ -224,7 +220,7 @@ class Command(BaseCommand):
                 'series': 'homologous'
             }
             VaccinationStatistics.objects.update_or_create(
-                site=site,
+                site_series=f'{site}_homologous',
                 defaults=vacc_defaults
             )
 
@@ -240,17 +236,32 @@ class Command(BaseCommand):
                 'serious_ae': adverse_events[1],
                 'special_ae': adverse_events[2],
                 'total': sum(adverse_events),
-                'series': 'homologous'
+                'series': 'homologous',
+                'site': site,
             }
             AdverseEvents.objects.update_or_create(
-                site=site,
+                site_series=f'{site}_homologous',
+                defaults=adverse_defaults
+            )
+
+            adverse_events = progress_reports.site_adverse_events(
+                subject_identifiers=progress_reports.heterologous_enrols, site_id=site_id)
+            adverse_defaults = {
+                'ae': adverse_events[0],
+                'serious_ae': adverse_events[1],
+                'special_ae': adverse_events[2],
+                'total': sum(adverse_events),
+                'series': 'heterologous',
+                'site': site,
+            }
+            AdverseEvents.objects.update_or_create(
+                site_series=f'{site}_heterologous',
                 defaults=adverse_defaults
             )
 
             # demographics
             demographics = progress_reports.site_demographics(
-                subject_identifiers=homologous_list, site_id=site)
-
+                subject_identifiers=homologous_list, site_id=site_id)
             demographics_defaults = {
                 'male': demographics[0],
                 'female': demographics[1],
@@ -260,10 +271,27 @@ class Command(BaseCommand):
                 'pos_preg': demographics[5],
                 'pos_covid': demographics[6],
                 'pos_diabetes': demographics[7],
-                'series': 'Homologous',
+                'series': 'homologous',
             }
             DemographicsStatistics.objects.update_or_create(
-                site=site,
+                site_series=f'{site}_homologous',
                 defaults=demographics_defaults
             )
-            # heterologous_vaccinations = progress_reports.vaccinations_per_product
+            
+            demographics = progress_reports.site_demographics(
+                subject_identifiers=progress_reports.heterologous_enrols, site_id=site_id)
+            demographics_defaults = {
+                'male': demographics[0],
+                'female': demographics[1],
+                'hiv_pos': demographics[2],
+                'hiv_neg': demographics[3],
+                'hiv_ind': demographics[4],
+                'pos_preg': demographics[5],
+                'pos_covid': demographics[6],
+                'pos_diabetes': demographics[7],
+                'series': 'heterologous',
+            }
+            DemographicsStatistics.objects.update_or_create(
+                site_series=f'{site}_heterologous',
+                defaults=demographics_defaults
+            )
