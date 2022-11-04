b7afc94d67b9612869dd29b7413b2bf70d0b06b6
Samuel Kabelo <samuelkabelo1@gmail.com>
2022-11-04 10:14:58 +0200

    updated off study

--- esr21/management/commands/create_offstudy.py
+++ esr21/management/commands/create_offstudy.py
@@ -10,11 +10,6 @@ class Command(BaseCommand):
 
     subject_offstudy_model = 'esr21_prn.subjectoffstudy'
     onschedule_model = 'esr21_subject.onschedule'
-    informed_consent_model = 'esr21_subject.informedconsent'
-
-    @property
-    def informed_consent_model_cls(self):
-        return django_apps.get_model(self.informed_consent_model)
 
     @property
     def subject_offstudy_cls(self):
@@ -26,29 +21,27 @@ class Command(BaseCommand):
     
 
     def handle(self, *args, **kwargs):
-        consent_identifiers = self.informed_consent_model_cls.objects.values_list(
+        
+        
+        onschedule_idx = self.onschedule_cls.objects.values_list(
                 'subject_identifier', flat=True).distinct()
         
-        for consent_identifier in consent_identifiers:
-            
-            onschedule_obj = self.onschedule_cls.objects.filter(
-                subject_identifier=consent_identifier)
-            
-            if onschedule_obj:
-                sid = onschedule_obj.first().subject_identifier
-                obj_dict = {}
-                try:
-                    self.subject_offstudy_cls.objects.get(
-                        subject_identifier=sid)
-                except self.subject_offstudy_cls.DoesNotExist:
-                    
-                    obj_dict.update(
-                        completed_study=NO,
-                        offstudy_date=get_utcnow().date(),
-                        reason='sponsor_terminated',
-                    )
-                    
-                    subject_offstudy =  self.subject_offstudy_cls(subject_identifier=sid,**obj_dict)
-                    subject_offstudy.save()   
-                else:
-                    pass
+        print(onschedule_idx.count())
+        
+        for sid in onschedule_idx:
+            obj_dict = {}
+            try:
+                self.subject_offstudy_cls.objects.get(
+                    subject_identifier=sid)
+            except self.subject_offstudy_cls.DoesNotExist:
+                
+                obj_dict.update(
+                    completed_study=NO,
+                    offstudy_date=get_utcnow().date(),
+                    reason='sponsor_terminated',
+                )
+                
+                subject_offstudy =  self.subject_offstudy_cls(subject_identifier=sid,**obj_dict)
+                subject_offstudy.save()   
+            else:
+                pass
